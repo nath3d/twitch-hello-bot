b8a59d6eef9b2d309695711daf12c7ddf9f10e83
nath3d <marecaux.nathan@gmail.com>
2022-05-09 20:51:00 +0200

    Init

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const tmi = require('tmi.js');
+const client = new tmi.Client({
+	options: { debug: true, messagesLogLevel: "info" },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: process.env.TWITCH_USERNAME,
+		password: process.env.TWITCH_OAUTHTOKEN
+	},
+	channels: [ process.env.TWITCH_CHANNEL ]
+});
+
+var processed = [ ]
+var users = process.env.TWITCH_USERS.split(',')
+
+client.connect().catch(console.error);
+client.on('message', (channel, tags, message, self) => 
+{
+    if(tags.username == process.env.TWITCH_USERNAME) return;
+    if(!(users.includes(tags.username) || tags.mod)) return;
+    if(processed.includes(tags.username)) return;
+
+    processed.push(tags.username);
+    if(tags.mod)
+        client.say(channel, `@${tags.username}, bjr bstiHey bstiHey`);
+    else
+        client.say(channel, `@${tags.username}, bjr bstiHey`);
+});
